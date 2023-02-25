298cdf3698b7ce5cf74f340553586d9406470606
dineshgowda24 <dinesh240894@gmail.com>
2023-02-25 12:01:07 +0530

    Fix failing tests.

--- source/player/human/human_test.go
+++ source/player/human/human_test.go
@@ -7,15 +7,24 @@ import (
 	"github.com/dineshgowda24/tic-tac-toe/source/player"
 )
 
+type ReadWriteNopCloser struct {
+	*bytes.Buffer
+}
+
+func (r *ReadWriteNopCloser) Close() error {
+	return nil
+}
+
 func TestNewHuman(t *testing.T) {
-	human := NewHuman(player.O, "Dinesh", bytes.NewReader([]byte("Sample Input")))
+
+	human := NewHuman(player.O, "Dinesh", &ReadWriteNopCloser{bytes.NewBuffer([]byte("Sample Input"))})
 	if human == nil {
 		t.Error("human can not be nil")
 	}
 }
 
 func TestHumanPlay(t *testing.T) {
-	human := NewHuman(player.O, "Dinesh", bytes.NewReader([]byte("1\n2\n3\n")))
+	human := NewHuman(player.O, "Dinesh", &ReadWriteNopCloser{bytes.NewBuffer([]byte("1\n2\n3\n"))})
 	if human == nil {
 		t.Error("human can not be nil")
 		t.FailNow()
